8f04c99640899b02135729d28cb720e1c37eb6fc
James Conway <2247492c@student.gla.ac.uk>
2018-03-09 15:32:16 +0000

    Began work on article.html

--- lit/urls.py
+++ lit/urls.py
@@ -7,5 +7,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'search/$', views.search, name='search'),
+    url(r'^article/(?P<article_name_slug>[\w\-]+)/$',
+        views.show_article, name='show_article'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
