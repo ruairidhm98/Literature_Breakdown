12b0c40298f492bcb542233856a019cf04e3fe1a
Scott Duncan <2137254d@student.gla.ack.uk>
2018-03-14 16:17:14 +0000

    Search HTML tweak

--- lit/views.py
+++ lit/views.py
@@ -17,6 +17,8 @@ def index(request):
     article_list_trending = Article.objects.order_by('-rating')[:5]
     article_list_new = Article.objects.order_by('date_published')[:5]
     category_list = Category.objects.all()
+    short_story = Articles.objects.filter(category_list="Short Story")
+    
     context_dict = {'articles_new': article_list_new,
                     'articles_trending' : article_list_trending,
                     'categories': category_list}
