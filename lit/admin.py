006294a7fa17fac7d6bd49e72e58e36972f0054c
“Ruairidh <2250079m@student.gla.ac.uk>
2018-03-14 14:05:52 +0000

    Added more stuff to DB, favourties etc

--- lit/admin.py
+++ lit/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from lit.models import Article, Comment, Snippet, Category, UserProfile
 from lit.models import *
 
 # Register your models here.
 
+admin.site.register(UserProfile)
 admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(Snippet)
 admin.site.register(Category)
-admin.site.register(UserProfile)
+admin.site.register(Favourites)
 
