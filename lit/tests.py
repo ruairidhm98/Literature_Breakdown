8297ee093acdcaa8748b11dd2bd8c4fb63e6ca1b
ScottDuncan <2137254d@student.gla.ac.uk>
2018-03-22 14:15:59 +0000

    Added test

--- lit/tests.py
+++ lit/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from lit.models import Article,UserProfile
 # Create your tests here.
+
+class ArticleMethodTest(TestCase):
+    def test_ensure_view_are_positive(selfs):
+        """
+
+        ensure_views_are_positive should results True fo
+        where views are zero or positive
+        :return:
+        """
+
+        art = Article(title='test',views=-1)
+        art.save()
+        seld.assertEqual((art.views>=0),True)
