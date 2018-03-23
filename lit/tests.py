5eee96616145e383d6ad1ee3460e769f9bf53787
ScottDuncan <2137254d@student.gla.ac.uk>
2018-03-23 10:01:41 +0000

    Got first test working

--- lit/tests.py
+++ lit/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from lit.models import Article,UserProfile
+from django.contrib.auth.models import User
 # Create your tests here.
 
 class ArticleMethodTest(TestCase):
-    def test_ensure_view_are_positive(selfs):
+    def test_ensure_view_are_positive(self):
         """
 
         ensure_views_are_positive should results True fo
@@ -11,6 +12,13 @@ class ArticleMethodTest(TestCase):
         :return:
         """
 
-        art = Article(title='test',views=-1, author=UserProfile.objects.filter(name=''))
+        user = User(username='test',password='test')
+        user.save()
+
+        user_prof = UserProfile(name='test',user=user)
+        user_prof.save()
+
+        art = Article(title='test',views=-1,author=user_prof)
         art.save()
-        seld.assertEqual((art.views>=0),True)
+        self.assertEqual((art.views>=0),True)
+
